a0797185b4ed944067d5886aa6909e2dbfb28599
K8691 <k8691@student.jamk.fi>
2021-03-26 14:49:12 +0200

    dokumentointi

--- dev_portfolio/js/script.js
+++ dev_portfolio/js/script.js
@@ -1,6 +1,7 @@
 var mybutton = document.getElementById("toTop");
 
 // BACK TO TOP BUTTON
+// https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
 // When the user scrolls down 50px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
@@ -12,6 +13,8 @@ function scrollFunction() {
   }
 }
 
+// when button is clicked, scroll to top
 function returnToTop() {
+  document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
